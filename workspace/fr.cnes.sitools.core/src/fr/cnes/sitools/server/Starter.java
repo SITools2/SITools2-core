aa3f9d5b1541eac38f22657e5ad33b29f0422e71
b.fiorito <b.fiorito@akka.eu>
2013-03-19 18:13:53 +0100

    add .gitignore to core sitools + dir logs relative in starter and sitools-reference.properties

--- workspace/fr.cnes.sitools.core/src/fr/cnes/sitools/server/Starter.java
+++ workspace/fr.cnes.sitools.core/src/fr/cnes/sitools/server/Starter.java
@@ -275,7 +275,7 @@ public final class Starter {
     // ============================
     // Logging configuration
 
-    String logConfigFile = settings.getString("Starter.Logging.configFile");
+    String logConfigFile = settings.getRootDirectory() + settings.getString("Starter.Logging.configFile");
     File loggingConfigFile = new File(logConfigFile);
     if (!loggingConfigFile.exists()) {
       System.err.println("Config file not found :" + logConfigFile);
@@ -287,7 +287,7 @@ public final class Starter {
     // ============================
     // Logs access
 
-    String logOutputFile = settings.getString("Starter.LogService.outputFile");
+    String logOutputFile = settings.getRootDirectory() + settings.getString("Starter.LogService.outputFile");
     String logLevelName = settings.getString("Starter.LogService.levelName");
     String logFormat = settings.getString("Starter.LogService.logFormat");
     String logName = settings.getString("Starter.LogService.logName");
@@ -297,7 +297,7 @@ public final class Starter {
 
     component.setLogService(logService);
 
-    String appLogOutputFile = settings.getString("Starter.AppLogService.outputFile");
+    String appLogOutputFile = settings.getRootDirectory() + settings.getString("Starter.AppLogService.outputFile");
     String appLogLevelName = settings.getString("Starter.AppLogService.levelName");
     String appLogFormat = settings.getString("Starter.AppLogService.logFormat");
     String appLogName = settings.getString("Starter.AppLogService.logName");
