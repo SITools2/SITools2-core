8654a52926bcb368f272a22ffa5b90faa99313d2
m.gond <m.gond@akka.eu>
2014-02-21 14:40:34 +0100

    exit 1 for error

--- workspace/sitools-build/scripts/clone-tag.sh
+++ workspace/sitools-build/scripts/clone-tag.sh
@@ -5,5 +5,5 @@
 git clone ${1} ${2}
 cd ${2}
 #check that the tag exists
-[ "`git tag | grep ^${3}$`" != "${3}" ] && echo "TAG ${3} does not exists, aborting" && exit 0;
+[ "`git tag | grep ^${3}$`" != "${3}" ] && echo "TAG ${3} does not exists, aborting" && exit 1;
 git checkout ${3}
