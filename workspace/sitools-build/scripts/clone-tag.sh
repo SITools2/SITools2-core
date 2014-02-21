0d975fccb75afc92068a1a5167935690c7506e0c
m.gond <m.gond@akka.eu>
2014-02-21 12:04:47 +0100

    test with test tag exists

--- workspace/sitools-build/scripts/clone-tag.sh
+++ workspace/sitools-build/scripts/clone-tag.sh
@@ -4,4 +4,6 @@
 #$3 correspond au nom du tag à récupérer
 git clone ${1} ${2}
 cd ${2}
+#check that the tag exists
+[ "`git tag | grep ^${3}$`" != "${3}" ] && echo "TAG ${3} does not exists, aborting" && exit 0;
 git checkout ${3}
