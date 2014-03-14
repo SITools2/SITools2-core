3220eb8c6b65d4be8ee47106acf96c717cac9986
m.gond <m.gond@akka.eu>
2014-03-14 14:37:53 +0100

    Copyright 2010-2014 workspace

--- workspace/client-public/js/widgets/ckeditor/sitoolsPlugins/datasetBrowser.js
+++ workspace/client-public/js/widgets/ckeditor/sitoolsPlugins/datasetBrowser.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2010-2013 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ * Copyright 2010-2014 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
  * 
  * This file is part of SITools2.
  * 
