db0b801b7fc700aaa282ef533c9ea0d09bad57f9
m.gond <m.gond@akka.eu>
2014-03-19 15:13:49 +0100

    trace collection application

--- workspace/fr.cnes.sitools.core/src/fr/cnes/sitools/collections/AbstractCollectionsResource.java
+++ workspace/fr.cnes.sitools.core/src/fr/cnes/sitools/collections/AbstractCollectionsResource.java
@@ -80,7 +80,6 @@ public abstract class AbstractCollectionsResource extends SitoolsResource {
    * @return Representation
    */
   public final Representation getRepresentation(Response response, MediaType media) {
-    getLogger().info(media.toString());
     if (media.isCompatible(MediaType.APPLICATION_JAVA_OBJECT)) {
       return new ObjectRepresentation<Response>(response);
     }
