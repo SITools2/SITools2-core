d2d89abe9c5a0a6a2a3c2a6a295e6a5d2d71e73f
Thierry Chevallier <tx.chevallier@akka.eu>
2014-03-21 10:17:08 +0100

    directory log filter

---
+++ workspace/fr.cnes.sitools.core/src/fr/cnes/sitools/proxy/DirectoryLogFilter.java
@@ -0,0 +1,50 @@
+package fr.cnes.sitools.proxy;
+
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.data.Method;
+import org.restlet.routing.Filter;
+
+/**
+ * DirectoryLogFilter
+ *  Filter to manage logs on directory proxy resources (CSS/FTL/CGU)
+ *  
+ * @author tx.chevallier
+ */
+public class DirectoryLogFilter extends Filter {
+
+  @Override
+  protected void afterHandle(Request request, Response response) {
+    // super.afterHandle(request, response);
+    String description = this.getNext().getDescription();
+    LogRecord record;
+
+    if (response.getStatus().isError()) {
+      record = new LogRecord(Level.INFO, "cannot access " + description.toUpperCase() + " editor  ");
+    }
+    else {
+      if ((Boolean) response.getAttributes().get("IS_DIRECTORY_TARGET")) {
+        record = new LogRecord(Level.FINE, "view of the " + description.toUpperCase() + " editor");
+      }
+      else {
+        if (request.getMethod().getName().equals(Method.GET.getName())) {
+          record = new LogRecord(Level.FINE, "edit the " + description.toUpperCase() + " file in the editor");
+        }
+        else if (request.getMethod().getName().equals(Method.PUT.getName())) {
+          record = new LogRecord(Level.INFO, "update the " + description.toUpperCase() + " file in the editor");
+        }
+        else {
+          record = new LogRecord(Level.FINE, request.getMethod().getName() + " on " + description.toUpperCase()
+              + " file");
+        }
+      }
+
+    }
+    response.getAttributes().put("LOG_RECORD", record);
+
+  }
+
+}
