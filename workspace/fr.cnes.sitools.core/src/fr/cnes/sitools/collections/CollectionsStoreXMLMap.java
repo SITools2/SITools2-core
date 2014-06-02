4efc66d6ccffe237bef999eab590c12b05b8b061
Thierry Chevallier <tx.chevallier@akka.eu>
2014-06-02 15:19:56 +0200

    update collection store xml map

--- workspace/fr.cnes.sitools.core/src/fr/cnes/sitools/collections/CollectionsStoreXMLMap.java
+++ workspace/fr.cnes.sitools.core/src/fr/cnes/sitools/collections/CollectionsStoreXMLMap.java
@@ -1,6 +1,8 @@
 package fr.cnes.sitools.collections;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -8,6 +10,9 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.restlet.Context;
 
 import fr.cnes.sitools.collections.model.Collection;
+import fr.cnes.sitools.common.model.ResourceCollectionFilter;
+import fr.cnes.sitools.common.model.ResourceComparator;
+import fr.cnes.sitools.order.model.Order;
 import fr.cnes.sitools.persistence.XmlMapStore;
 
 
@@ -17,7 +22,6 @@ public class CollectionsStoreXMLMap extends XmlMapStore<Collection> implements C
   
   /** default location for file persistence */
   private static final String COLLECTION_NAME = "collections";
-  
   
   public CollectionsStoreXMLMap(Class<Collection> cl, File location, Context context) {
     super(cl, location, context);
@@ -46,5 +50,46 @@ public class CollectionsStoreXMLMap extends XmlMapStore<Collection> implements C
   }
   
   
+  @Override
+  public Collection update(Collection collection) {
+    Collection result = null;
+    getLog().info("Updating collection");
+    
+    Map<String, Collection> map = getMap();
+    Collection current = map.get(collection.getId());
+
+    result = current;
+    current.setId(collection.getId());
+    current.setName(collection.getName());
+    current.setDescription(collection.getDescription());
+    current.setDataSets(collection.getDataSets());
+
+    map.put(collection.getId(), current);
+    
+    return result;
+  }
+  
+  /**
+   * Sort the list (by default on the name)
+   * 
+   * @param result
+   *          list to be sorted
+   * @param filter
+   *          ResourceCollectionFilter with sort properties.
+   */
+  public void sort(List<Collection> result, ResourceCollectionFilter filter) {
+    if ((filter != null) && (filter.getSort() != null) && !filter.getSort().equals("")) {
+      Collections.sort(result, new ResourceComparator<Collection>(filter) {
+        @Override
+        public int compare(Collection arg0, Collection arg1) {
+
+          String s1 = (String) arg0.getName();
+          String s2 = (String) arg1.getName();
+
+          return super.compare(s1, s2);
+        }
+      });
+    }
+  }
 
 }
