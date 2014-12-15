83aaf7ceee740b48628eb6ab835482397a3c8211
m.gond <m.gond@akka.eu>
2014-12-15 11:35:56 +0100

    install gulp during minify process on installizpack

--- workspace/sitools-install-izpack/conf/scripts/minifyjs.bat
+++ workspace/sitools-install-izpack/conf/scripts/minifyjs.bat
@@ -1,6 +1,8 @@
 :: %1 correspond to the project to build
 
 cd %1
+echo "install gulp into %1"
+npm install gulp gulp-concat gulp-uglifyjs
 echo "build of %1 javascript files"
 :: build des fichiers javascripts
 gulp
