83fa33471bd3d60154c05b0ddef65bd88ca6ec67
m.gond <m.gond@akka.eu>
2014-12-15 11:17:55 +0100

    Fix izpack install with gulp

--- workspace/sitools-install-izpack/conf/scripts/minifyjs.sh
+++ workspace/sitools-install-izpack/conf/scripts/minifyjs.sh
@@ -6,16 +6,22 @@ cd ${1}
 ## ${1} correspond au root directory
 
 cd ${1}/workspace/client-admin
+echo "install gulp into client-admin"
+npm install gulp gulp-concat gulp-uglifyjs
 echo "build client-admin javascript files"
 ## build des fichiers javascripts
 gulp
 
 cd ${1}/workspace/client-user-3.0
+echo "install gulp into client-user-3.0"
+npm install gulp gulp-concat gulp-uglifyjs
 echo "build client-user javascript files"
 ## build des fichiers javascripts
 gulp
 
 cd ${1}/workspace/client-portal-3.0
+echo "install gulp into client-portal-3.0"
+npm install gulp gulp-concat gulp-uglifyjs
 echo "build client-portal javascript files"
 ## build des fichiers javascripts
 gulp
