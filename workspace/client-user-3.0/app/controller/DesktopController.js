b120842f82ffbb12db4a36b70f17d93f12feded8
m.gond <m.gond@akka.eu>
2014-12-18 15:37:44 +0100

    Fix window saving

--- workspace/client-user-3.0/app/controller/DesktopController.js
+++ workspace/client-user-3.0/app/controller/DesktopController.js
@@ -26,6 +26,73 @@ Ext.define('sitools.user.controller.DesktopController', {
     views : [ 'desktop.DesktopView' ],
 
     init : function () {
+    	this.control({
+            'menu#saveMenuWindow menuitem#saveUser' : {
+                click : function (menuItem) {
+                    var menu = menuItem.up("menu");
+                    var window = menu.window;
+                    var component = window.items.items[0];
+
+                    var componentSettings = component._getSettings();
+                    window.saveSettings(componentSettings, false);
+                }
+
+            },
+            'menu#saveMenuWindow menuitem#savePublic' : {
+                click : function (menuItem) {
+                    var menu = menuItem.up("menu");
+                    var window = menu.window;
+                    var component = window.items.items[0];
+
+                    var componentSettings = component._getSettings();
+                    window.saveSettings(componentSettings, true);
+                }
+
+            },
+            'window tool#save' : {
+                click : function (tool, e, eOpts) {
+                    var window = tool.up("window");
+
+                    var saveLabel = Ext.create('Ext.menu.Item', {
+                        text : i18n.get('label.chooseSaveType'),
+                        plain : false,
+                        canActivate : false,
+                        cls : 'userMenuCls'
+                    });
+
+                    var menuForms = Ext.create("Ext.menu.Menu", {
+                        border : false,
+                        plain : true,
+                        width : 260,
+                        closeAction : 'hide',
+                        itemId : 'saveMenuWindow',
+                        window : window,
+                        items : [saveLabel, {
+                            xtype : 'menuseparator',
+                            separatorCls : 'customMenuSeparator'
+                        }, {
+                            text: i18n.get("label.myself"),
+                            cls : 'menuItemCls',
+                            iconCls : 'saveUserIcon',
+                            itemId : 'saveUser',
+                        },
+                        {
+                            text: i18n.get("label.publicUser"),
+                            cls : 'menuItemCls',
+                            iconCls : 'savePublicIcon',
+                            itemId : 'savePublic',
+                        }]
+                    });
+
+                    var x = e.getX() + 5;
+                    var y = e.getY() + 5;
+
+                    menuForms.showAt([x, y]);
+                }
+            }
+    	});
+
+
         this.getApplication().on('projectLoaded', this.realInit, this);
     	this.callParent(arguments);
     },
