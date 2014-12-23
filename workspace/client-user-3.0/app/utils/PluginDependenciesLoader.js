54ff6a218ec2ee3ca12233a40e81687fa1ed35cf
m.gond <m.gond@akka.eu>
2014-12-23 16:10:00 +0100

    Update PluginDependenciesLoader file

--- workspace/client-user-3.0/app/utils/PluginDependenciesLoader.js
+++ workspace/client-user-3.0/app/utils/PluginDependenciesLoader.js
@@ -27,36 +27,47 @@
 Ext.define('sitools.user.utils.PluginDependenciesLoader', {
         
     requires : [
-                //modules
-                'sitools.user.modules.AddToCartModule',
-                'sitools.user.modules.ContentEditor',
-                'sitools.user.modules.ContentViewerModule',
-                'sitools.user.modules.DatasetExplorer',
-                'sitools.user.modules.DataStorageExplorer',
-                'sitools.user.modules.FeedsProjectModule',
-                'sitools.user.modules.FormAsMenuModule',
-                'sitools.user.modules.FormModule',
-                'sitools.user.modules.ProjectDescription',
-                'sitools.user.modules.ProjectService',
-                //gui services
-                
-                
-                
-                
-                
-                
-                //form components
-                
-                
-                
-                
-                
-                
-                
-                
-                //dataset views
-                
-                
-                
-                ]
+        //modules
+        'sitools.user.modules.AddToCartModule',
+        'sitools.user.modules.ContentEditor',
+        'sitools.user.modules.ContentViewerModule',
+        'sitools.user.modules.DatasetExplorer',
+        'sitools.user.modules.DataStorageExplorer',
+        'sitools.user.modules.FeedsProjectModule',
+        'sitools.user.modules.FormAsMenuModule',
+        'sitools.user.modules.FormModule',
+        'sitools.user.modules.ProjectDescription',
+        'sitools.user.modules.ProjectService',
+        //gui services
+        'sitools.user.component.datasets.services.AddToCartService',
+        'sitools.user.component.datasets.services.ColumnsDefinitionService',
+        'sitools.user.component.datasets.services.DisplaySelectionCartService',
+        'sitools.user.component.datasets.services.FilterService',
+        'sitools.user.component.datasets.services.PlotService',
+        'sitools.user.component.datasets.services.RecordDetailService',
+        'sitools.user.component.datasets.services.SorterService',
+        'sitools.user.component.datasets.services.WindowImageZoomerService',
+        ////form components
+        'sitools.public.forms.components.AbstractConeSearch',
+        'sitools.public.forms.components.BooleanCheckbox',
+        'sitools.public.forms.components.CheckBox',
+        'sitools.public.forms.components.ComboBox',
+        'sitools.public.forms.components.ConeSearchCartesien',
+        'sitools.public.forms.components.ConeSearchPGSphere',
+        'sitools.public.forms.components.DateBetween',
+        'sitools.public.forms.components.Hl',
+        'sitools.public.forms.components.Image',
+        'sitools.public.forms.components.Label',
+        'sitools.public.forms.components.ListBoxMultiple',
+        'sitools.public.forms.components.ListBox',
+        'sitools.public.forms.components.MapPanel',
+        'sitools.public.forms.components.NumberField',
+        'sitools.public.forms.components.NumericBetween',
+        'sitools.public.forms.components.OneOrBetween',
+        'sitools.public.forms.components.Radio',
+        'sitools.public.forms.components.TextField',
+        ////dataset views
+        'sitools.user.component.datasets.dataviews.CartoView',
+        'sitools.user.component.datasets.dataviews.Livegrid'
+    ]
 });
