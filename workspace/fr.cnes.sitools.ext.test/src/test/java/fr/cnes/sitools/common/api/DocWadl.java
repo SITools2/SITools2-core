a281c472047f7ea7898df6d610b6a5e20c5f3b60
m.gond <m.gond@akka.eu>
2015-07-17 10:56:14 +0200

    [ext-test] Fix test and code refactoring

---
+++ workspace/fr.cnes.sitools.ext.test/src/test/java/fr/cnes/sitools/common/api/DocWadl.java
@@ -0,0 +1,89 @@
+ /*******************************************************************************
+ * Copyright 2010-2014 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of SITools2.
+ *
+ * SITools2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SITools2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SITools2.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package fr.cnes.sitools.common.api;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
+ /**
+  * Class to generate WADL documentation
+  *
+  *
+  * @author m.marseille (AKKA Technologies)
+  */
+ public class DocWadl {
+
+   /** Application name */
+   private String applicationName = null;
+
+   /** XML File output */
+   private File docWadl;
+
+   /** HTML File output */
+   private File docHtml;
+
+   /**
+    * Constructor
+    * @param appName the name of the web application
+    */
+   public DocWadl(String appName) {
+     super();
+     this.applicationName = appName;
+     try {
+       String docUrl = "./documentation/wadl/";
+       String baseUrl = docUrl + this.applicationName;
+       File wadlDirectory = new File(baseUrl);
+       if (!wadlDirectory.exists()) {
+         wadlDirectory.mkdirs();
+       }
+       if (appName != null && !appName.equals("")) {
+         this.docWadl = new File(baseUrl + "/" + this.applicationName + ".xml");
+         this.docWadl.createNewFile();
+         this.docHtml = new File(baseUrl + "/" + this.applicationName + ".html");
+         this.docHtml.createNewFile();
+       }
+     }
+     catch (IOException e) {
+       e.printStackTrace();
+     }
+   }
+
+   /**
+    * Get the print stream for Wadl
+    * @return a stream where wadl can be written.
+    * @throws java.io.FileNotFoundException if file is not found
+    */
+   public PrintStream getWadlPrintStream() throws FileNotFoundException {
+     PrintStream ps = new PrintStream(docWadl);
+     return ps;
+   }
+
+   /**
+    * Get the print stream for Html
+    * @return a stream where HTML can be written.
+    * @throws java.io.FileNotFoundException if file is not found
+    */
+   public PrintStream getHtmlPrintStream() throws FileNotFoundException {
+     PrintStream ps = new PrintStream(docHtml);
+     return ps;
+   }
+
+ }
---
+++ workspace/fr.cnes.sitools.ext.test/src/test/java/fr/cnes/sitools/common/utils/XmlRecordsModel.java
@@ -0,0 +1,111 @@
+ /*******************************************************************************
+ * Copyright 2010-2014 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of SITools2.
+ *
+ * SITools2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SITools2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SITools2.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package fr.cnes.sitools.common.utils;
+
+ import fr.cnes.sitools.datasource.jdbc.model.Record;
+
+ import java.util.List;
+
+ public class XmlRecordsModel {
+
+   private Integer total;
+
+   private Integer count;
+
+   private Integer offset;
+
+   private List<Record> records;
+
+   /**
+    * Gets the total value
+    *
+    * @return the total
+    */
+   public Integer getTotal() {
+     return total;
+   }
+
+   /**
+    * Sets the value of total
+    *
+    * @param total
+    *          the total to set
+    */
+   public void setTotal(Integer total) {
+     this.total = total;
+   }
+
+   /**
+    * Gets the count value
+    *
+    * @return the count
+    */
+   public Integer getCount() {
+     return count;
+   }
+
+   /**
+    * Sets the value of count
+    *
+    * @param count
+    *          the count to set
+    */
+   public void setCount(Integer count) {
+     this.count = count;
+   }
+
+   /**
+    * Gets the offset value
+    *
+    * @return the offset
+    */
+   public Integer getOffset() {
+     return offset;
+   }
+
+   /**
+    * Sets the value of offset
+    *
+    * @param offset
+    *          the offset to set
+    */
+   public void setOffset(Integer offset) {
+     this.offset = offset;
+   }
+
+   /**
+    * Gets the records value
+    *
+    * @return the records
+    */
+   public List<Record> getRecords() {
+     return records;
+   }
+
+   /**
+    * Sets the value of records
+    *
+    * @param records
+    *          the records to set
+    */
+   public void setRecords(List<Record> records) {
+     this.records = records;
+   }
+
+ }
