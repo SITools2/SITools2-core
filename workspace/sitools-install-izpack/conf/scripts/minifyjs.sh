9deacd010a60bffe5dad5e386d4db99faf31dc1b
Hubert Nouhen <hubert.nouhen@akka.eu>
2016-08-26 16:42:36 +0200

    fix gulp

--- workspace/sitools-install-izpack/conf/scripts/minifyjs.sh
+++ workspace/sitools-install-izpack/conf/scripts/minifyjs.sh
@@ -7,14 +7,14 @@ cd ${1}
 
 cd ${1}/workspace/client-admin
 echo "install gulp into client-admin"
-npm install gulp gulp-concat gulp-uglifyjs
+npm install gulp gulp-concat gulp-uglify pump gulp-rename
 echo "build client-admin javascript files"
 ## build des fichiers javascripts
 gulp
 
 cd ${1}/workspace/client-user-3.0
 echo "install gulp into client-user-3.0"
-npm install gulp gulp-concat gulp-uglifyjs
+npm install gulp gulp-concat gulp-uglify pump gulp-rename
 echo "build client-user javascript files"
 ## build des fichiers javascripts
 gulp
@@ -22,7 +22,7 @@ gulp build-with-plugins
 
 cd ${1}/workspace/client-portal-3.0
 echo "install gulp into client-portal-3.0"
-npm install gulp gulp-concat gulp-uglifyjs
+npm install gulp gulp-concat gulp-uglify pump gulp-rename
 echo "build client-portal javascript files"
 ## build des fichiers javascripts
 gulp
