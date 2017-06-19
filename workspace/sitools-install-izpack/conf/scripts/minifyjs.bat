2b104c47e10b1b7151088d3ccf1ccd4f3daee08d
Hubert Nouhen <hubert.nouhen@akka.eu>
2017-06-19 16:54:03 +0200

    [libraries] update uglifyjs library deprecated name to uglify

--- workspace/sitools-install-izpack/conf/scripts/minifyjs.bat
+++ workspace/sitools-install-izpack/conf/scripts/minifyjs.bat
@@ -2,14 +2,14 @@
 
 cd %1/workspace/client-admin
 echo "install gulp into client-admin"
-call npm install gulp gulp-concat gulp-uglifyjs
+call npm install gulp gulp-concat gulp-uglify
 echo "build of client-admin javascript files"
 :: build des fichiers javascripts de client-admin
 call gulp
 
 cd %1/workspace/client-user-3.0
 echo "install gulp into client-user-3.0"
-call npm install gulp gulp-concat gulp-uglifyjs
+call npm install gulp gulp-concat gulp-uglify
 echo "build of client-user-3.0 javascript files"
 :: build des fichiers javascripts
 call gulp
@@ -17,7 +17,7 @@ call gulp build-with-plugins
 
 cd %1/workspace/client-portal-3.0
 echo "install gulp into client-portal-3.0"
-call npm install gulp gulp-concat gulp-uglifyjs
+call npm install gulp gulp-concat gulp-uglify
 echo "build of client-portal-3.0 javascript files"
 :: build des fichiers javascripts
 call gulp
